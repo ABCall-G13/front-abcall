8e53ad04de0a7b165529cfc1bf970b3dddbdb3f8
sebaschb <sebastianchaconbenitez@gmail.com>
2024-11-17 23:25:28 -0500

    Add internationalization support with i18next and language selection

--- src/components/Select/LanguageSelector.tsx
+++ src/components/Select/LanguageSelector.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const LanguageSelector: React.FC = () => {
+    const { i18n } = useTranslation();
+
+    const changeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        i18n.changeLanguage(e.target.value);
+    };
+
     return (
-        <select className="selector">
+        <select className="selector" onChange={changeLanguage} value={i18n.language}>
             <option value="es">Español</option>
             <option value="en">Inglés</option>
         </select>
