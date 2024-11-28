5369d6dc263ed30aa15ead9a51f255d5d6d8f980
Liceth Yaneth <liceth.rozo@finkargo.com>
2024-11-28 06:16:47 -0500

    Add tests for handling valid and invalid message documents in Chatbot component

--- src/pages/Chatbot/Chatbot.test.tsx
+++ src/pages/Chatbot/Chatbot.test.tsx
@@ -103,7 +103,7 @@ describe('Send message', () => {
     });
 });
 
-describe('Receive message', () => {
+describe('Receive message document valid', () => {
     test('should receive a message', async () => {
         // Mockear respuesta de Axios
         mockAxiosInstance.get.mockResolvedValueOnce({
@@ -142,6 +142,60 @@ describe('Receive message', () => {
         // Esperar a que el mensaje de respuesta sea recibido
         await waitFor(() =>
             expect(screen.getByText('Por favor, ingresa tu número de identificación.')).toBeInTheDocument()
+        );
+    });
+});
+
+describe('Receive message document invalid', () => {
+    test('should receive a message', async () => {
+        // Mockear respuesta de Axios
+        mockAxiosInstance.get.mockResolvedValueOnce({
+            data: {
+                solutions: [
+                    { solucion: 'Reinicia tu router.' },
+                    { solucion: 'Verifica los cables.' },
+                ],
+            },
+        });
+
+        render(<Chatbot />);
+
+        // Buscar y hacer clic en el botón para abrir el chatbot
+        const openChatbotButton = screen.getByText('Chat en línea');
+        fireEvent.click(openChatbotButton);
+
+        // Esperar a que el contenido del chatbot sea visible
+        await waitFor(() =>
+            expect(screen.getByText('Asistente Virtual')).toBeInTheDocument()
+        );
+
+        // Buscar el campo de texto y escribir un mensaje
+        const messageInput = screen.getByPlaceholderText('Escribe un mensaje...');
+        fireEvent.change(messageInput, { target: { value: 'CC' } });
+
+        // Buscar y hacer clic en el botón para enviar el mensaje
+        const sendMessageButton = screen.getByRole('button', { name: /send icon/i });
+        fireEvent.click(sendMessageButton);
+
+        // Esperar a que el mensaje sea enviado
+        await waitFor(() =>
+            expect(screen.getByText('CC')).toBeInTheDocument()
+        );
+
+        // Buscar el campo de texto y escribir un mensaje
+        fireEvent.change(messageInput, { target: { value: '123456r789' } });
+
+        // Buscar y hacer clic en el botón para enviar el mensaje
+        fireEvent.click(sendMessageButton);
+
+        // Esperar a que el mensaje sea enviado
+        await waitFor(() =>
+            expect(screen.getByText('123456r789')).toBeInTheDocument()
+        );
+
+        // Esperar a que el mensaje de respuesta sea recibido
+        await waitFor(() =>
+            expect(screen.getByText('El número de identificación debe ser un valor numérico. Inténtalo de nuevo.')).toBeInTheDocument()
         );
     });
 });
